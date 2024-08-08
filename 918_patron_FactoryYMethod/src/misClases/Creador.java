abbf6d8805cbbbad06e9571d121a3693fe0618ac
DPSColinKiefer1 <delpinhosaldana@hotmail.com>
2024-08-08 13:34:48 -0500

    ultimo

--- 918_patron_FactoryYMethod/src/misClases/Creador.java
+++ 918_patron_FactoryYMethod/src/misClases/Creador.java
@@ -2,9 +2,11 @@ package misClases;
 public abstract class Creador { //crea una clase abstracta llamada Creador, se hace abstracta para que las clases hijas puedan 
 	//modificar sus metodos a conveniencia
     public abstract Producto factoryMethod();    // Método abstracto que debe ser implementado por las clases hijas. 
-    public void realizarOperacion(){  // Método concreto que utiliza el retorno del método abstracto.
+    public void realizarOperacion(){  // Método concreto que utiliza el retorno del método abstracto, osea, el resultado retornado
+    	//las otras clases(la creacion de un objeto de la clase hija) es usado por este metodo que es de la clase padre, osea, esta
+    	//presente en todas las clases hijas
         Producto producto=factoryMethod();
-        //este crea una variable de la clase Producto que almacenara el retorno de factoryMethod
+        //este crea una variable de la clase Producto que almacenara el retorno de factoryMethod, osea, un objeto de ambas clases
         producto.usar(); //este llama al metodo "usar" de la clase Producto a la que los objetos de su tipo tienen permiso
     }
 }
--- 918_patron_FactoryYMethod/src/poo23850patronfactoryymethod/Poo23850patronFactoryYMethod.java
+++ 918_patron_FactoryYMethod/src/poo23850patronfactoryymethod/Poo23850patronFactoryYMethod.java
@@ -2,7 +2,7 @@ package poo23850patronfactoryymethod;
 import misClases.*;
 public class Poo23850patronFactoryYMethod {
     public static void main(String[] args) {
-    	Creador creadorA=new CreadorConcretoA();
+    	Creador creadorA=new CreadorConcretoA(); //
     	creadorA.realizarOperacion();
     	Creador creadorB=new CreadorConcretoB();
     	creadorB.realizarOperacion();    	
